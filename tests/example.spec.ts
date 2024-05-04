9a185e7c7ff4b5693f9ef00284ad5b8ac4e0e326
LiviuSosu <sosuliviu@gmail.com>
2024-05-04 12:02:00 +0200

    wip

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,19 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+// test('has title', async ({ page }) => {
+//   await page.goto('http://localhost:3000/');
 
-  // Expect a title "to contain" a substring.
+//   // Expect a title "to contain" a substring.
   
-  await expect(page).toHaveTitle(/Career track/);
-});
+//   await expect(page).toHaveTitle(/Career track/);
+// });
 
-test('get started link', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('http://localhost:3000/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Home' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Home' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('paragraph')).toContainText("home page");
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('paragraph')).toContainText("home page");
+// });
