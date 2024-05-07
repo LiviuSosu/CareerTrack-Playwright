4f2cee90037d702a7cbc347777cbac6c18d31831
LiviuSosu <sosuliviu@gmail.com>
2024-05-07 14:58:56 +0200

    full update like a bo$$

--- tests/login-flow.spec.ts
+++ tests/login-flow.spec.ts
@@ -1,17 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-// test('when accessing the profile page unautentificated then redirect to login and to find the Login button', async ({ page }) => {
-//     await page.goto('http://localhost:3000/pages/profile');
+test('when accessing the profile page unautentificated then redirect to login and to find the Login button', async ({ page }) => {
+    await page.goto('http://localhost:3000/pages/profile');
   
-//     let loginButton = await page.getByRole('button', { name: 'Login' });
+    let loginButton = await page.getByRole('button', { name: 'Login' });
     
-//     await expect(loginButton).toBeVisible();
-//   });
+    await expect(loginButton).toBeVisible();
+  });
 
   test('when succesfull login the profile page is displayed', async ({ page }) => {
+
+    await page.goto('http://localhost:3000/pages/profile');
+    await page.waitForURL('http://localhost:3000/pages/login');
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+
     await page.goto('http://localhost:3000/pages/login');
   
-    await page.getByLabel('User name').fill('admin2');
+    await page.getByLabel('User name').fill('admin@gmail.com');
     await page.getByLabel('Password').fill('Password@123');
     await page.getByRole('button', { name: 'Login' }).click();
     
