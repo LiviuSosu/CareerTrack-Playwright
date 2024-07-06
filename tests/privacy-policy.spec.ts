0e3ab66bfc3c9b94c000e3c4344546e45b0e9447
LiviuSosu <sosuliviu@gmail.com>
2024-07-06 09:04:19 +0200

    wip

--- tests/privacy-policy.spec.ts
+++ tests/privacy-policy.spec.ts
@@ -8,5 +8,15 @@ test('when the "here" link from the cookies banner is clicked it is redirected t
     await page.getByText('here').click();
 
     await page.waitForURL(PrivacyPolicyUrl);
-    await expect(page.getByText('privacy policy')).toBeVisible();
+    await expect(page.getByText('Cookie management overview')).toBeVisible();
+})
+
+test('when the "Consent" button is being clicked the banner dissapears', async ({ page }) => {
+    let bannerText = 'only essential cookies';
+
+    await page.goto(HomePageUrl);
+    await page.waitForURL(HomePageUrl);
+    await expect(page.getByText(bannerText)).toBeVisible();
+    await page.getByText('Consent').click();
+    await expect(page.getByText(bannerText)).not.toBeVisible();
 })
